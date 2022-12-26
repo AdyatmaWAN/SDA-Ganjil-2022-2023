5be5b9fc566a3cdde8fbf6d37fa4948ef4fb1d69
AdyatmaNugrahaYudha <awan2403@gmail.com>
2022-12-26 16:23:46 +0700

    TP3

--- TP3/src/Main.java
+++ TP3/src/Main.java
@@ -7,9 +7,6 @@ public class Main {
     static FastReader in;
     static PrintWriter out;
     
-    static long[][] time = new long[1001][1001];
-    
-    
 
     public static void main(String[] args) {
         InputStream inputStream = System.in;
@@ -22,64 +19,54 @@ public class Main {
     
         ArrayList < ArrayList < AdjListNode > > graphW = new ArrayList<>(); //inisiasi graph benteng
         ArrayList < ArrayList < AdjListNodeLength > > graphL = new ArrayList<>();
+        ArrayList < AdjListNodeLength > list = new ArrayList<>(); //arraylist isi semua tunnel
         
-        for (int i = 0; i <= inPost; i++) {
+        for (int i = 0; i <= inPost; i++) { //inisiasi graph agar bisa memiliki menyimpan tunnel
             graphW.add(new ArrayList<>());
             graphL.add(new ArrayList<>());
         }
     
-        for (int i = 0; i < inTunn; i++) {
+        for (int i = 0; i < inTunn; i++) { //input tunnel
             int A = in.nextInt(), B = in.nextInt(), L = in.nextInt(), S = in.nextInt();
             graphW.get(A).add(new AdjListNode(B, S, A));
             graphW.get(B).add(new AdjListNode(A, S, B));
             graphL.get(A).add(new AdjListNodeLength(B, L));
             graphL.get(B).add(new AdjListNodeLength(A, L));
+            list.add(new AdjListNodeLength(A, L, 0, B));
         }
         
-//        for (int a = 1; a <= inPost; a++) {
-//            for (int b = 0; b < graphW.get(a).size(); b++) {
-//                System.out.println(a + " " + graphW.get(a).get(b).getVertex());
-//            }
-//        }
-        
-        int inDwarves = in.nextInt();
+        int inDwarves = in.nextInt(); //inisiasi penyimpanan kurcaci
     
-        ArrayList<Integer> dwarves = new ArrayList<>(); //arraylist dan inisiasi benteng diserang
+        ArrayList<Integer> dwarves = new ArrayList<>();
         for (int i = 0; i < inDwarves; i++) {
             int F = in.nextInt();
             dwarves.add(F);
         }
         
-        int inQuery = in.nextInt();
+        int inQuery = in.nextInt(); //inisiasi banyak query
         
         for (int i = 0; i < inQuery; i++) {
             String inCMD = in.next();
-            if (inCMD.equals("KABUR")) {
+            if (inCMD.equals("KABUR")) { //jika query kabur maka akan melakukan traversal sampai ketemu node tujuan
                 int inFrom = in.nextInt();
                 int inTo = in.nextInt();
                 
                 long[] distance = traversal(inPost, graphW, inFrom, inTo, out);
                 
                 out.println(distance[inTo]);
-                
-//                for (int j = 0; j <= inPost; j++) {
-//                    out.print(distance[j] + " ");
-//                }
-//                out.println();
-                
             
-            } else if (inCMD.equals("SIMULASI")) {
+            } else if (inCMD.equals("SIMULASI")) { //jika melakukan simulasi maka akan mencari shortest dari node 0 yang terhubung ke semua exit
                 graphL.set(0, new ArrayList<>());
                 int inExit = in.nextInt();
                 int[] exit = new int[inExit];
-                for (int j = 0; j < inExit; j++) {
+                for (int j = 0; j < inExit; j++) { //inisiasi exit
                     exit[j] = in.nextInt();
                     graphL.get(0).add(new AdjListNodeLength(exit[j], 0));
                 }
                 
-                long[] distance = dijkstra(inPost, graphL, 0);
+                long[] distance = dijkstra(inPost, graphL, 0); //djikstra shortest path
                 long length = 0;
-                for (int j = 0; j < inDwarves; j++) {
+                for (int j = 0; j < inDwarves; j++) { //mencari kurcaci dengan waktu terlamam untuk kelaur
                     if (distance[dwarves.get(j)] >= length) {
                         length =  distance[dwarves.get(j)];
                     }
@@ -87,17 +74,47 @@ public class Main {
                 out.println(length);
                 
                 
-            } else {
-            
+            } else { //jika query super maka akan melakukan djikstra dari asal, tujuan 1 dan tujuan 2 kurcaci lalu
+                int inSrc = in.nextInt();
+                int inDest1 = in.nextInt();
+                int inDest2 = in.nextInt();
+    
+                long dist1 = Long.MAX_VALUE;
+                long dist2 = Long.MAX_VALUE;
+    
+                long[] distanceN1 = dijkstra(inPost, graphL, inSrc);
+                long[] distanceN2 = dijkstra(inPost, graphL, inDest1);
+                long[] distanceN3 = dijkstra(inPost, graphL, inDest2);
+    
+                for (AdjListNodeLength n : list) { //membandingkan semua tunnel yang akan di skip dan dicari yang terkecil
+                    if (dist1 > distanceN1[n.getLast()] + distanceN2[n.getVertex()]) {
+                        dist1 = distanceN1[n.getLast()] + distanceN2[n.getVertex()];
+                    }
+                    if (dist1 > distanceN1[n.getVertex()] + distanceN2[n.getLast()]) {
+                        dist1 = distanceN1[n.getVertex()] + distanceN2[n.getLast()];
+                    }
+                    if (dist2 > distanceN2[n.getLast()] + distanceN3[n.getVertex()]) {
+                        dist2 = distanceN2[n.getLast()] + distanceN3[n.getVertex()];
+                    }
+                    if (dist2 > distanceN2[n.getVertex()] + distanceN3[n.getLast()]) {
+                        dist2 = distanceN2[n.getVertex()] + distanceN3[n.getLast()];
+                    }
+                }
+                
+                //membandingkan mana yang lebih kecil, yang di skip sebelum destinasi 1 atau setelah destinasi 1
+                if (dist1 + distanceN3[inDest1] < dist2 + distanceN1[inDest1]) {
+                    out.println(dist1 + distanceN3[inDest1]);
+                } else {
+                    out.println(dist2 + distanceN1[inDest1]);
+                }
             }
-            
             
         }
         
         out.close();
     }
 
-    static class AdjListNode implements Comparable<AdjListNode> { //adj list for the graph
+    static class AdjListNode implements Comparable<AdjListNode> { //adj list for the graph (width)
 
         int vertex, width, last; //attrbutes of the node
 
@@ -116,32 +133,46 @@ public class Main {
         }
     }
 
-    static class AdjListNodeLength implements Comparable<AdjListNodeLength> { //adj list for the graph
+    static class AdjListNodeLength implements Comparable<AdjListNodeLength> { //adj list for the graph (length)
 
-        int vertex, length; //attrbutes of the node
+        int vertex, length, isSuper, last; //attrbutes of the node
 
         AdjListNodeLength(int v, int l) { //constructor
             vertex = v;
             length = l;
+            isSuper = 0;
         }
+    
+        AdjListNodeLength(int v, int l, int s, int ls) { //constructor
+            vertex = v;
+            length = l;
+            isSuper = s;
+            last = ls;
+        }
+        
         int getVertex() { return vertex; } //getters
         int getLength() { return length; }
+        int getIsSuper() { return isSuper; }
+        int getLast() { return last; }
 
         public int compareTo(AdjListNodeLength other) { //compare methods
             return this.length - other.length;
         }
     }
 
-    public static long[] dijkstra(int V, ArrayList<ArrayList<AdjListNodeLength> > graph, int src) { //djikstra for length
-        long[] distance = new long[V+1];
+    
+    //https://www.geeksforgeeks.org/dijkstras-algorithm-for-adjacency-list-representation-greedy-algo-8/
+    //djikstra shortest path algorithm modified from gfg
+    public static long[] dijkstra(int V, ArrayList<ArrayList<AdjListNodeLength> > graph, int src) { //djikstra shortest path to get distance to all node
+        long[] distance = new long[V+1]; //initiate distance array
         for (int i = 0; i <= V; i++)
             distance[i] = Long.MAX_VALUE;
         distance[src] = 0;
 
-        BinaryHeap<AdjListNodeLength> pq = new BinaryHeap<>(); //width compared
-        pq.insert(new AdjListNodeLength(src, 0));
+        BinaryHeap<AdjListNodeLength> pq = new BinaryHeap<>(); //heap for the priority queue
+        pq.insert(new AdjListNodeLength(src, 0)); //insert adjlistnode to the heap
 
-        while (pq.size() > 0) {
+        while (pq.size() > 0) { //while heap is not empty bakal menambah adjlistnode ke heap dan mengupdate distance dari setiap node yang dilalui
             AdjListNodeLength current = pq.remove();
 
             for (AdjListNodeLength n : graph.get(current.getVertex())) {
@@ -157,34 +188,31 @@ public class Main {
                 }
             }
         }
-        // If you want to calculate distance from source to
-        // a particular target, you can return
-        // distance[target]
-        return distance;
+        return distance; //return distance array
     }
     
+    //modified from previous dijkstra to update when entering node and stop when reaching destination
     public static long[] traversal(int V, ArrayList<ArrayList<AdjListNode> > graph, int src, int dst, PrintWriter out) { //djikstra for length
-        long[] distance = new long[V+1];
+        long[] distance = new long[V+1]; //initiate array
         for (int i = 0; i <= V; i++)
             distance[i] = Long.MAX_VALUE;
         distance[src] = 0;
         
-        BinaryHeap<AdjListNode> pq = new BinaryHeap<>(); //width compared
+        BinaryHeap<AdjListNode> pq = new BinaryHeap<>(); //initiate heap
         pq.insert(new AdjListNode(src, 0, 0));
         int x = 0;
         
         while (pq.size() > 0) {
             AdjListNode current = pq.remove();
-            
-//            out.println(current.getVertex() + " vertex now");
+            //modifikasi ada pada jika node awal maka akan mengupdate dengan width yang lebih kecil
+            //jika node baru memiliki width yang lebih besar maka akan diisi dengan width node sebelumnya
+            //namun jika node sebelumnya adalah source maka  widthnya tidak akan dirubah
             if (current.getWidth() < distance[current.getLast()] || current.getLast() == src) {
                 if (x > 0) {
-//                    out.println(current.getVertex() + " " + current.getWidth());
                     distance[current.getVertex()] = current.getWidth();
                 }
             } else {
                 if (x > 0) {
-//                    out.println(current.getVertex() + " " + distance[current.getLast()]);
                     distance[current.getVertex()] = distance[current.getLast()];
                 }
             }
@@ -194,47 +222,41 @@ public class Main {
             }
             
             for (AdjListNode n : graph.get(current.getVertex())) {
-//                out.print(n.getWidth() + " " + distance[n.getVertex()] + " if");
-//                out.println();
                 if (n.getWidth() < distance[n.getVertex()]) {
-//                    out.println(n.getVertex() + " add to heap");
                     pq.insert(new AdjListNode(n.getVertex(), n.getWidth(), n.getLast()));
                 }
             }
-//            out.println();
             x++;
             
         }
-        // If you want to calculate distance from source to
-        // a particular target, you can return
-        // distance[target]
         return distance;
     }
+    
 }
 class BinaryHeap<T extends Comparable<T>> {
-    ArrayList<T> data;
+    ArrayList<T> data; //arraylist to store the heap
 
     public BinaryHeap() {
         data = new ArrayList<T>();
-    }
+    } //initiate heap
 
-    public BinaryHeap(ArrayList<T> arr) {
+    public BinaryHeap(ArrayList<T> arr) { //initiate heap
         data = arr;
         heapify();
     }
 
-    public T peek() {
+    public T peek() { //see the top of the heap
         if (data.isEmpty())
             return null;
         return data.get(0);
     }
 
-    public void insert(T value) {
+    public void insert(T value) { //insert value to the heap
         data.add(value);
         percolateUp(data.size() - 1);
     }
 
-    public T remove() {
+    public T remove() { //remove the top of the heap
         T removedObject = peek();
 
         if (data.size() == 1)
@@ -248,7 +270,7 @@ class BinaryHeap<T extends Comparable<T>> {
         return removedObject;
     }
 
-    private void percolateDown(int idx) {
+    private void percolateDown(int idx) { //percolate down the heap
         T node = data.get(idx);
         int heapSize = data.size();
 
@@ -274,7 +296,7 @@ class BinaryHeap<T extends Comparable<T>> {
         }
     }
 
-    private void percolateUp(int idx) {
+    private void percolateUp(int idx) { // percolate up the heap
         T node = data.get(idx);
         int parentIdx = getParentIdx(idx);
         while (idx > 0 && node.compareTo(data.get(parentIdx)) < 0) {
